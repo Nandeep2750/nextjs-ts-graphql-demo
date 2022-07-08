4d170f2f95edb6bfecc386781feae88d6621d866
nandeep21 <nandeep@openxcellinc.com>
2022-07-08 18:39:43 +0530

    Add login page and use formik & yup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+// add bootstrap css 
+import 'bootstrap/dist/css/bootstrap.css'
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
